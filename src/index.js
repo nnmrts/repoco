276aad479ce281e46d14343a0d7aaaaeb07c3929
nnmrts <nanomiratus@gmail.com>
2019-12-13 07:07:21 +0100

    removed test function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 // @flow
 import expressions from "./expressions.js";
 
-const filteredExpressions = Object.fromEntries(Object.entries(expressions).filter(([
-	code,
-	expression
-]: [
-	string,
-	mixed
-]): boolean => {
-	if (expression instanceof RegExp) {
-		return expression.source !== "^$";
-	}
-	return false;
-}));
-
-console.info(filteredExpressions);
+console.info(expressions);
