8103d34f1461355a9f1df38d6af6d9f710aca80c
nnmrts <nanomiratus@gmail.com>
2019-12-13 12:17:03 +0100

    added helper to twn

--- src/expressions/asia/eastern-asia/twn.js
+++ src/expressions/asia/eastern-asia/twn.js
@@ -2,5 +2,6 @@
 // Taiwan
 // n{3}[-n{2}]
 // impossible first digit is 0
+import full2NumberDigits from "../../../helpers/full/number/full-2-number-digits.js";
 
-export default /^[1-9][0-9]{2}(-[0-9]{2})?$/;
+export default new RegExp(`^[1-9]${full2NumberDigits}(-${full2NumberDigits})?$`);
