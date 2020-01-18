6e3446876de8f7c30748ccd1ba4ff9ee5ce7aab9
nnmrts <nanomiratus@gmail.com>
2020-01-18 22:20:54 +0100

    added support for "at index" syntax

---
+++ src/transpiler/apply-constraints/index-to-positional.js
@@ -0,0 +1,20 @@
+// @flow
+/**
+ *
+ *
+ * @param {number} [number=0]
+ * number
+ * @returns {string}
+ * string
+ */
+export default (number: number = 0): string => [
+	"first",
+	"second",
+	"third",
+	"fourth",
+	"fifth",
+	"sixth",
+	"seventh",
+	"eigth",
+	"ninth"
+][number];
---
+++ src/transpiler/parser/operations/unfold/index-constraint-actions.js
@@ -0,0 +1,42 @@
+// @flow
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+import { Node } from "ohm-js";
+
+import indexToPositional from "../../../apply-constraints/index-to-positional.js";
+
+type ConstraintKeys = "indexExcluder" | "indexTolerator";
+
+export type Constraint = {|
+	type: ConstraintKeys,
+	position: string,
+	amount: ?string,
+	digits: string[]
+|};
+
+type ConstraintFunction = () => Constraint;
+
+export default Object.fromEntries<ConstraintKeys, ConstraintFunction>([
+	"indexExcluder",
+	"indexTolerator"
+].map<[
+	ConstraintKeys,
+	ConstraintFunction
+]>((rule: ConstraintKeys): [
+	ConstraintKeys,
+	ConstraintFunction
+] => [
+	rule,
+	(
+		constraintWord: Node,
+		amount: Node,
+		digitWord: Node,
+		atIndex: Node,
+		beWord: Node,
+		digits: Node
+	): Constraint => ({
+		type: rule,
+		position: indexToPositional(Number(atIndex.children[2].sourceString)),
+		amount: amount.children.length ? amount.children[0].children[0].sourceString : null,
+		digits: digits.unfold()[0]
+	})
+]));
