dbf987922fbe0b2f58984b622f79cf9a45bce097
nnmrts <nanomiratus@gmail.com>
2020-01-18 20:52:52 +0100

    removed experimental definition syntax

--- src/expressions/europe/northern-europe/irl.js
+++ src/expressions/europe/northern-europe/irl.js
@@ -2,11 +2,10 @@
 // Republic of Ireland|Ireland
 // cn{2} x{4}
 // impossible first digits are B,G,I,J,L,M,O,Q,S,U,Z
-// impossible fourth to seventh digits are B,G,I,J,L,M,O,Q,S,U,Z
-
+// impossible four digits at index 4 are B,G,I,J,L,M,O,Q,S,U,Z
 // or
 // D6W x{4}
-// impossible fourth to seventh digits are B,G,I,J,L,M,O,Q,S,U,Z
+// impossible four digits at index 4 are B,G,I,J,L,M,O,Q,S,U,Z
 import full2NumberDigits from "../../../helpers/full/number/full-2-number-digits.js";
 
 export default new RegExp(`^((?![BGIJLMOQSUZ])[A-Z]${full2NumberDigits}|D6W) ((?![BGIJLMOQSUZ])[A-Z0-9]){4}$`);
