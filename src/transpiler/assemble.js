d128059366836bd47392e5dc9ff1d7cc408438b7
nnmrts <nanomiratus@gmail.com>
2020-01-18 20:57:09 +0100

    cleaned up added more prefix and suffix "killers" added support for optional digits tried to fix final replacers

--- src/transpiler/assemble.js
+++ src/transpiler/assemble.js
@@ -31,21 +31,29 @@ export default (variants: PostVariants): RegExp => {
 	let commonPrefix = newVariants.length === 1 ? "" : findCommonPrefix(newVariants);
 	let commonSuffix = newVariants.length === 1 ? "" : reverseString(findCommonPrefix(newVariants.map(reverseString)));
 
-	if (commonPrefix.match(/\(|\)/)) {
+	// TODO: allow if number of parentheses is even
+	if (commonPrefix.match(/\(|\)|\[|\]|\{|\}/)) {
 		commonPrefix = "";
 	}
-	if (commonSuffix.match(/\(|\)/)) {
+	if (commonSuffix.match(/\(|\)|\[|\]|\{|\}/)) {
 		commonSuffix = "";
 	}
 
-	return new RegExp(`^${[
-		commonPrefix,
-		`(${newVariants.map((variant: string): string => variant.replace(commonPrefix, "").replace(commonSuffix, "")).join("|")})`,
-		commonSuffix
+	const assembledVariants = [
+		commonPrefix + (newVariants.length > 1 ? "(" : ""),
+		`${
+			newVariants
+				.map((variant: string): string => variant
+					.replace(commonPrefix, "")
+					.replace(commonSuffix, ""))
+				.join("|")
+		}`,
+		(newVariants.length > 1 ? ")" : "") + commonSuffix
 	]
 		.join("")
 		.replace(/\((\(\?!([[\]|\-0-9A-Z ])+\)\([[\]|\-{}()0-9A-Z ]+\))\)/g, "$1")
-		.replace(/^\(([^|]*?)\)$/, "$1")
-		.replace(/\(\?(=|!)((?:(?:[0-9]|\[(?:[0-9](?:-?[0-9]+)?)+\]){2}\|)+(?:[0-9]|\[(?:[0-9](?:-?[0-9]+)?)+\]){2})\)\((\[0-9\]\{2\})\)/g, "(?$1$2)$3")
-	}$`);
+		.replace(/^\((?!(?:(?:.*?)(?:\)\(|\|)(?:.*?)))(.*?)\)$/, "$1")
+		.replace(/\(\?(=|!)((?:(?:[0-9]|\[(?:[0-9](?:-?[0-9]+)?)+\]){2}\|)+(?:[0-9]|\[(?:[0-9](?:-?[0-9]+)?)+\]){2})\)\((\[0-9\]\{2\})\)/g, "(?$1$2)$3");
+
+	return new RegExp(`^${assembledVariants}$`);
 };
