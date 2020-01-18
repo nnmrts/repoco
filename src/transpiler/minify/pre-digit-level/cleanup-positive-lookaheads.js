1d3cb8e036012c5e9f778686406b6720ef988ef6
nnmrts <nanomiratus@gmail.com>
2020-01-18 21:50:02 +0100

    added more multi digit positive lookaheads cleanup

--- src/transpiler/minify/pre-digit-level/cleanup-positive-lookaheads.js
+++ src/transpiler/minify/pre-digit-level/cleanup-positive-lookaheads.js
@@ -1,4 +1,16 @@
 // @flow
 import type { Digit } from "../../types.js";
 
-export default (digit: Digit): Digit => digit.replace(/^\(\(\?=\(([0-9A-Z])\)\)\(\[(0-9|A-Z|0-9A-Z|A-Z0-9)\]\)\)$/, "$1");
+export default (digit: Digit): Digit => {
+	const regexes = [
+		/\(\(\?=\(([0-9A-Z])\)\)\(\[(0-9|A-Z|A-Z0-9)\]\)\)/g,
+		...Array.from(Array(8)).map((item: ?number, i: number): RegExp => new RegExp(`\\(\\?=(\\(([0-9A-Z]{${i + 2}}\\|)+[0-9A-Z]{${i + 2}}\\))\\)\\((\\[(0-9|A-Z|A-Z0-9)\\]){${i + 2}}\\)`, "g"))
+	];
+
+	let newDigit = digit;
+
+	for (let i = 0; i < regexes.length; i++) {
+		newDigit = newDigit.replace(regexes[i], "$1");
+	}
+	return newDigit;
+};
