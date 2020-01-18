ad1f91d47f5c8236e48361ad9cad009e6f895a08
nnmrts <nanomiratus@gmail.com>
2020-01-18 22:21:13 +0100

    added new actions to operations

--- src/transpiler/parser/operations/unfold.js
+++ src/transpiler/parser/operations/unfold.js
@@ -11,18 +11,24 @@ import digitList from "./unfold/digit-list.js";
 import or from "./unfold/or.js";
 import variant from "./unfold/variant.js";
 import variants from "./unfold/variants.js";
+import repeaterRange from "./unfold/repeater-range.js";
+import optional from "./unfold/optional.js";
+import indexConstraintActions from "./unfold/index-constraint-actions.js";
 
 export default Object.assign(
 	{
 		repeater,
+		repeaterRange,
 		pattern,
 		constraint,
 		digitList,
 		or,
+		optional,
 		variant,
 		variants
 	},
 	primitiveActions,
 	tokenActions,
-	constraintActions
+	constraintActions,
+	indexConstraintActions
 );
