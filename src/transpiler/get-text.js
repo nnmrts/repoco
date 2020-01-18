793bfb171d2c43a1fa974cb3b7c2d28974ecac70
nnmrts <nanomiratus@gmail.com>
2020-01-18 20:54:43 +0100

    converted to synchronous function

--- src/transpiler/get-text.js
+++ src/transpiler/get-text.js
@@ -1,40 +1,16 @@
 // @flow
 import fs from "fs";
-import readline from "readline";
 
 /**
  *
  *
  * @param {string} path
  * file path
- * @returns {Promise<string>}
+ * @returns {string}
  * the text
  */
-export default async(path: string): Promise<string> => new Promise(
-	(resolve: (string) => void) => {
-		const fileStream = fs.createReadStream(path);
-
-		const rl = readline.createInterface({
-			input: fileStream,
-			crlfDelay: Infinity
-		});
-
-		const lines = [];
-
-		rl.on("line", (line: string) => {
-			if (line[0] === "/" && line[1] === "/") {
-				if (line[3] !== "@") {
-					lines.push(line.slice(3));
-				}
-			}
-			else {
-				rl.close();
-				rl.removeAllListeners();
-			}
-		});
-
-		rl.on("close", async() => {
-			resolve(lines.slice(1).join(" "));
-		});
-	}
-);
+export default (path: string): string => fs.readFileSync(path, "utf8").split("\n")
+	.filter((line: string, index: number, array: string[]): boolean => !!line.match(/^\/\/\s/) && (index === 0 || !!array[index - 1].match(/^\/\/\s/)) && !line.startsWith("// @"))
+	.slice(1)
+	.map((line: string): string => line.replace(/^\/\/\s/, ""))
+	.join(" ");
