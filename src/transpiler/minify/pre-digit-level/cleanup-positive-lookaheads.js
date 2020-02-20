bf4280cfda6927d3d00675e7637b6017458e54d1
nnmrts <nanomiratus@gmail.com>
2020-02-20 23:55:41 +0100

    refactored handling of positive lookaheads

--- src/transpiler/minify/pre-digit-level/cleanup-positive-lookaheads.js
+++ src/transpiler/minify/pre-digit-level/cleanup-positive-lookaheads.js
@@ -6,15 +6,13 @@ import { type Digit } from "../../types.js";
  * @param digit
  */
 export default (digit: Digit): Digit => {
-	const regexes = [
-		/\(\(\?=\(([0-9A-Z])\)\)\(\[(0-9|A-Z|A-Z0-9)\]\)\)/g,
-		...Array.from(Array(8)).map((item: ?number, i: number): RegExp => new RegExp(`\\(\\?=(\\(([0-9A-Z]{${i + 2}}\\|)+[0-9A-Z]{${i + 2}}\\))\\)\\((\\[(0-9|A-Z|A-Z0-9)\\]){${i + 2}}\\)`, "g"))
-	];
+	const regexes = [/\(\(\?=\(((?:[0-9A-Z])(?:\|[0-9A-Z])*)\)\)\((?:\[(0-9|A-Z|0-9A-Z)\])\)\)/g, ...Array(8).fill().map((value: void, index: number): RegExp => new RegExp(`\\(\\(\\?=(\\((?:[0-9A-Z]{${index + 2}})(?:\\|[0-9A-Z]{${index + 2}})*\\))\\)\\((?:\\[(0-9|A-Z|0-9A-Z)\\]){${index + 2}}\\)\\)`, "g"))];
 
 	let newDigit = digit;
 
-	for (let i = 0; i < regexes.length; i++) {
-		newDigit = newDigit.replace(regexes[i], "$1");
+	for (const regex of regexes) {
+		newDigit = `(${newDigit.replace(regex, "$1")})`;
 	}
+
 	return newDigit;
 };
